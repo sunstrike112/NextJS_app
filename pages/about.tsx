383fa2b5c055b88079a0b34f93bdc728a0529bab
PhapTDQ <phaptran112@gmail.com>
2022-12-28 10:01:45 +0700

    NextJS: 02-05 Automatic Static Optimization

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,23 @@
-import * as React from 'react';
+import * as React  from 'react';
+import { useEffect }  from 'react';
+import { useRouter } from 'next/router';
 
 export interface AboutPageProps {}
 
 export default function AboutPage(props: AboutPageProps) {
+  const router = useRouter()
+  console.log('About query: ', router.query)
+
+  useEffect(() => {
+    //Check query available
+
+  }, [])
+
   return <div>About Page</div>
+}
+
+export async function getServerSideProps() {
+  return {
+    props: {}
+  }
 }

a1a483f2c4ebff95044f3b952d3f72a6fa657acb
PhapTDQ <phaptdq@bap.jp>
2023-02-07 09:34:24 +0700

    NextJS: 02-09 Server Side Rendering with cache

--- pages/post/param.tsx
+++ pages/post/param.tsx
@@ -1,13 +1,22 @@
-import { useRouter } from "next/router"
-import * as React from "react"
+import { useRouter } from "next/router";
+import * as React from "react";
 
-export interface ParamPageProps { }
+export interface ParamPageProps {}
 
 export default function ParamPage(props: ParamPageProps) {
-  const router = useRouter()
+  const router = useRouter();
 
-  return <div>
-    <h1>Param Page</h1>
-    <p>Query: {JSON.stringify(router.query)}</p>
-  </div>
+  return (
+    <div>
+      <h1>Param Page</h1>
+      <p>Query: {JSON.stringify(router.query)}</p>
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  return {
+    props: {},
+  };
 }

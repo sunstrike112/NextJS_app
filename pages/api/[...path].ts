5a9d9c78c1e2be9df6fd88bc5758129814f0eb42
PhapTDQ <phaptdq@bap.jp>
2023-02-08 16:35:29 +0700

    NextJS: 03-05 Handle login & logout with cookies

--- pages/api/[...path].ts
+++ pages/api/[...path].ts
@@ -1,27 +1,37 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import httpProxy from "http-proxy"
+import type { NextApiRequest, NextApiResponse } from "next";
+import httpProxy from "http-proxy";
+import Cookies from "cookies";
 
 export const config = {
   api: {
     bodyParser: false,
-  }
-}
+  },
+};
 
-const proxy = httpProxy.createProxyServer()
+const proxy = httpProxy.createProxyServer();
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  req.headers.cookie = ""
+  return new Promise((resolve) => {
+    const cookies = new Cookies(req, res)
+    const accessToken = cookies.get('access_token')
+    if (accessToken) {
+      req.headers.authorization = `Bearer ${accessToken}`
+    }
 
-  proxy.web(req, res, 
-    {
+    req.headers.cookie = "";
+    proxy.web(req, res, {
       target: process.env.API_URL,
       changeOrigin: true,
-      selfHandleResponse: false
-    })
+      selfHandleResponse: false,
+    });
 
+    proxy.once("proxyRes", () => {
+      resolve(true);
+    });
+  });
   // res.status(200).json({ name: 'PATH - match ALL' })
 }
